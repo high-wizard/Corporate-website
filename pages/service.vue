efda7a40c3bd9f0132513a3f210976712dff1f3b
sKawashima <bysk.333@gmail.com>
2020-02-24 16:18:56 +0900

    Make title section

---
+++ pages/service.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+  div
+    HeroView(headingText='SERVICE' headingSubText='実績紹介' imgSrc='/image/service.jpg')
+    .wrapper
+      section.services
+        Heading(type='secondary' text='Webサイト 開発・運用' centering='true')
+      section.services
+        Heading(type='secondary' text='複合システム・その他' centering='true')
+</template>
+
+<script>
+import HeroView from '@/components/organisms/HeroView'
+import Heading from '@/components/molecules/Heading'
+
+export default {
+  components: {
+    HeroView,
+    Heading
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+.wrapper
+  padding: 0 20px
+  color: #040E17
+
+.services:not(:first-of-type)
+  margin-top: 180px
+</style>
