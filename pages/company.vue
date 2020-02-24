70d59ff1925ab75152abe5b486cad68cb19f1a72
sKawashima <bysk.333@gmail.com>
2020-02-24 18:19:51 +0900

    Add gmap

--- pages/company.vue
+++ pages/company.vue
@@ -14,7 +14,7 @@
       CompanyOverview.companyOverviewTable
     section.access
       Heading(type='secondary' text='アクセス' :centering='true')
-
+      iframe.gmap(src='https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d5829.947560754659!2d141.362791!3d43.063019!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x5f0b2979fd36bd67%3A0xd3fc5c3dd6d8da9!2z44CSMDYwLTAwNDEg5YyX5rW36YGT5pyt5bmM5biC5Lit5aSu5Yy65aSn6YCa5p2x77yU5LiB55uuIO-8qO-8te-8p--8oSDvvK_vvKTvvK_vvLLvvKkg77yz77y177yp77y077yl!5e0!3m2!1sja!2sjp!4v1582535864154!5m2!1sja!2sjp' height='400' frameborder='0' style='border:0;' allowfullscreen='')
 </template>
 
 <script>
@@ -59,4 +59,7 @@ export default {
 
 .access
   margin-top: 120px
+  .gmap
+    margin-top: 60px
+    width: 100%
 </style>
