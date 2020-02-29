a5d7039b00097209457fba78c1e127df3f549efb
Shiranui Nui <shiranuisayaka@gmail.com>
2020-03-01 00:22:37 +0900

    Fix forgot break

--- pages/recruit.vue
+++ pages/recruit.vue
@@ -69,7 +69,7 @@
           p.text recruit@high-wizard.com<br>
             | 履歴書及び【職務経歴書 又は ポートフォリオ等実績の分かるPDF形式等の書類若しくはURL】をお送り下さい。
           Heading(type='tertiary' text='直接書類送付').heading
-          p.text 本社住所：北海道札幌市手稲区前田8条17丁目3-30
+          p.text 本社住所：北海道札幌市手稲区前田8条17丁目3-30<br>
             | 履歴書及び【職務経歴書 又は ポートフォリオ等実績の分かる印刷物】をお送り下さい。
         .item
           Heading(type='tertiary' text='業務委託契約でのお仕事をお探しの方へ')
